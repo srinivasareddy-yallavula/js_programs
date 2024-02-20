1d5bef208f87653ce0995741184af28a19ace047
srinivasareddy-yallavula <srinivasareddyyallavula76@gmail.com>
2024-02-20 16:29:01 +0530

    test commit

--- AnonymousFunctions.js
+++ AnonymousFunctions.js
@@ -19,3 +19,19 @@ processNumber(function(num){
 console.log("/*Arrow function*/")
 processNumber((numbers)=>{console.log(numbers);});
 // processNumber((values)=>{console.log(values*2);});
+
+console.log("--function in another function--")
+function f1(fun,x,y){
+    let result=fun(x,y);
+    return result;
+}
+var add= function(x,y){
+    return x+y;
+}
+var sub= function(x,y){
+    return x-y;
+}
+var result=f1(add,4,5);
+console.log(result);
+var result=f1(sub,6,3);
+console.log(result);
--- returnFunction.js
+++ returnFunction.js
@@ -10,3 +10,26 @@ var sayWelcome=greetingWishes("welcome");
 
 sayHello("kmss");
 sayWelcome("kmss");
+
+console.log("--return function from another function--")
+var isEven=function(x,y){
+    let sum=x+y;
+    console.log(sum);
+}
+function f1(){
+    console.log("inside f1 function");
+    return isEven;
+}
+function f2(){
+    console.log("inside f2 function");
+    return function(x,y){
+        let sum=x+y;
+        console.log(sum);
+    };
+}
+var evenfun=f1();
+var r1= evenfun(5,10);
+console.log(r1);
+var evenfunc=f2();
+var r2= evenfunc(10,5);
+console.log(r2);
