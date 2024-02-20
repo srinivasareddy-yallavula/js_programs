e6f4ee10a8adb2e804ed4a784270c21b03ed087b
srinivasareddy-yallavula <srinivasareddyyallavula76@gmail.com>
2024-02-20 14:23:01 +0530

    test commit

--- Arrays.js
+++ Arrays.js
@@ -65,3 +65,17 @@ function maxElement(arr){
 var arr=[1,3,5,4,2,2,4,1,9,11];
     var result=maxElement(arr);
     console.log(result);
+
+    console.log("--count number of zeros in an arr--")
+function numberOfZeros(arr){
+    let count=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==3){
+            count++;
+        }
+    }
+    return count;
+}
+var arr=[2,3,4,5,0,6,0,1,3,6,8,0]
+var result=numberOfZeros(arr);
+console.log(result);
